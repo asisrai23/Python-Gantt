f33805b9707fb548f422476a01faec36b563d738
A Norman (norman <A.Norman (norman@xael.org)>
2015-01-06 00:15:49 +0100

    adding TODOLIST.org

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 
 setup (
     name = 'python-gantt',
-    version = '0.2.1',
+    version = '0.2.2',
     author = 'Alexandre Norman',
     author_email = 'norman@xael.org',
     license ='gpl-3.0.txt',
