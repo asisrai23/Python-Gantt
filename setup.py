ce1940d2abe1039dbee9708112405ca2d29d1774
A Norman <A Norman@localhost>
2015-01-12 10:08:53 +0100

    bug correction in setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup (
-    name = 'python-gantt',
-    version = '0.3.6',
+    name = 'gantt',
+    version = '0.3.7',
     author = 'Alexandre Norman',
     author_email = 'norman@xael.org',
     license ='gpl-3.0.txt',
