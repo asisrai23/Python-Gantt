3f5d14b2058ba36f99290f2d0000638ebe6e841e
A Norman <A Norman@localhost>
2015-01-16 17:10:20 +0100

    update version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
 
 setup (
     name = 'python-gantt',
-    version = '0.3.7',
+    version = '0.3.8',
     author = 'Alexandre Norman',
     author_email = 'norman@xael.org',
     license ='gpl-3.0.txt',
