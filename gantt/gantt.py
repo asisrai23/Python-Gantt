e2ca5d1ea3e63f2afe595745771607dc362a0cfc
A Norman <A Norman@localhost>
2015-04-08 15:33:48 +0200

    one_line_for_task option

--- gantt/gantt.py
+++ gantt/gantt.py
@@ -27,8 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __author__ = 'Alexandre Norman (norman at xael.org)'
-__version__ = '0.3.14'
-__last_modification__ = '2015.03.24'
+__version__ = '0.3.15'
+__last_modification__ = '2015.04.08'
 
 import datetime
 import logging
