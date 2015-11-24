5d2d3c5fcee9930d10d056d1359d3137da8fc292
A Norman (norman <A.Norman (norman@xael.org)>
2015-11-24 23:14:08 +0100

    Correcting Bug #3 : Big SVG gets cut in Firefox/IE11 Change in Makefile for web publication

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
 
 setup (
     name = 'python-gantt',
-    version = '0.4.2',
+    version = '0.4.3',
     author = 'Alexandre Norman',
     author_email = 'norman@xael.org',
     license ='gpl-3.0.txt',
@@ -40,6 +40,7 @@ setup (
         ],
     packages=['gantt'],
     url = 'http://xael.org/pages/python-gantt-en.html',
+    bugtrack_url = 'https://bitbucket.org/xael/python-gantt',
     description = 'This is a python class to create gantt chart using SVG.',
     long_description=long_description,
     install_requires=[
